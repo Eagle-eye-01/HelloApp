52412db4f96927e9f5a8d3341468b91be6872d47
your@email.com <krishugouravchaurasia@gmail.com>
2026-03-22 00:40:58 +0530

    UC2 done

--- uc2/Greeting.java
+++ uc2/Greeting.java
@@ -3,7 +3,7 @@ public class Greeting {
         if (args.length > 0) {
             System.out.println("Hello " + args[0]);
         } else {
-            System.out.println("Hello User");
+            System.out.println("Hello  User");
         }
     }
 }
